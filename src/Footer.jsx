f8203f6404a5d75253ba4187166be1d3b83ce00a
devendrasingh24 <devendrap24singh@gmail.com>
2022-01-25 12:06:10 +0530

    my first upload

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <h6
+      className=" text-center text-white m-0 p-3"
+      style={{ backgroundColor: "blue" }}
+    >
+      copyright@online-casino-Game
+    </h6>
+  );
+}
